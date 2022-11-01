55c6cb102c892305b870b32738a3893ec78b03e1
Erin Bennett <ebennett@kehillah.org>
2022-11-01 13:53:17 -0700

    add drawing file and tasks

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# What are the digits of $\pi$
+# What are the digits of $\pi$?
+
+## Tasks
+
+1. [ ] TODO 1: In `drawing.py` modify the `draw_point` function so that it moves the turtle to the appropriate place before drawing the dot. **(3 points)** Test your code by running the file `drawing.py`. You should see 3 black dots and some pink text that says, "Hello, World!"
+2. [ ] TODO 2: In `drawing.py` modify the `write` function to change the color, font, or size of the text on the screen. **(3 points)** Test your code by running the file `drawing.py`.
+
+[ ] In `drawing.py` modify the `draw_point` function so that it takes an additional parameter `in_circle`. Change the color of the dot so that if `in_circle` is `True`, the dot is one color, but if `in_circle` is `False`, it's another color. **(3 points)** Modify the doc string for the `draw_point` function to explain the new parameter. **(1 point)** In the `main` function in `drawing.py`, there are 3 calls to the function `draw_point`. Update the function calls to include either `True` or `False` as the argument. Test your code by running the file `drawing.py`. **(2 points)**
---
+++ drawing.py
@@ -0,0 +1,68 @@
+import turtle
+
+def draw_point(x, y):
+    """
+    Draw a point at position x, y
+    
+    Parameters:
+    * `x` (float): x-axis position
+    * `y` (float): y-axis position
+    """
+    # Stop drawing lines where the turtle goes
+    turtle.up()
+
+    # Go to the position
+    # TODO 1 ------> YOUR CODE HERE!!!
+
+    # Stamp a dot at the turtle's current location
+    turtle.dot()
+
+def write(text):
+    """
+    Draw text in large letters in the middle of the screen
+    
+    Parameters:
+    * `text` (str): String to print on canvas
+    """
+    turtle.goto(0,0)
+    turtle.down()
+
+    # TODO 2 ------> Change color, font, or size of text
+
+    turtle.color("magenta")
+    turtle.write(
+        text,
+        font=("Courier", 64, "normal"),
+        align = "center"
+    )
+    turtle.up()
+
+def setup():
+    """
+    Setup turtle so that we only render at the end, rather than watching the
+    animation.
+    """
+    turtle.tracer(0,0)
+
+def finish():
+    """
+    End the turtle commands so that we render the image at the end and keep the
+    screen open until the user clicks on it.
+    """
+    turtle.hideturtle()
+    turtle.update()
+    turtle.exitonclick()
+
+if __name__ == "__main__":
+    setup()
+
+    # Draw a point at the origin
+    draw_point(0, 0)
+    # Draw a point at the upper right corner
+    draw_point(100, 100)
+    # Draw a point at the lower left corner
+    draw_point(-100, -100)
+
+    write("Hello, World!")
+
+    finish()
